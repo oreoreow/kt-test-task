0c904d8f04a98eceb80e8a7f82f5812438ea6525
denis.selditsin <denis.selditsin@ya.ru>
2022-12-11 20:15:01 +0300

    feat(products): create scheme

---
+++ src/Products/Domain/Entity/Product.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Users\Domain\Entity;
+
+use App\Users\Domain\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $weight = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
---
+++ src/Products/Domain/Repository/ProductRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Products\Domain\Repository;
+
+interface ProductRepositoryInterface
+{
+
+}
